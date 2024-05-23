b18de51f13a20b3277ad9a176b93b84ef41751b9
kunal <121377679+vkunal8013@users.noreply.github.com>
2024-05-23 15:47:22 +0530

    updated Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate, Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 
 const App = () => {
